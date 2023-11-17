f999f436ad3225e8bf30ef9a8c150d539898f140
zGiusee <giusebelforte@gmail.com>
2023-11-17 13:22:38 +0100

    Es done

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let number = 100;
+console.log(number); 
+
+
+
+const pi = 3.14;
+console.log(pi);
+
+let radius = 8;
+console.log(radius)
+
+let circle = radius * 2 * pi;
+console.log(circle)
+
+let name = `Carlo`;
+name = `Marco`;
+console.log(name);
+
