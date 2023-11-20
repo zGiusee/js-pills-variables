6f7f37d752c99c5f5346669f5e6807fcc7740fcb
zGiusee <giusebelforte@gmail.com>
2023-11-20 10:07:51 +0100

    Exercise 4b done

--- js/script.js
+++ js/script.js
@@ -1,29 +1,33 @@
 // Esercizio 1
-// let number = 100;
-// console.log(number); 
+let number = 100;
+console.log(number); 
 
 // Esercizio 2
-// const pi = 3.14;
-// console.log(pi);
+const pi = 3.14;
+console.log(pi);
 
 // Esercizio 2/B
-// let radius = 8;
-// console.log(radius)
+let radius = 8;
+console.log(radius)
 
 // Esercizio 2/C
-// let circle = radius * 2 * pi;
-// console.log(circle)
+let circle = radius * 2 * pi;
+console.log(circle)
 
 // Esercizio 3
-// let name = `Carlo`;
-// name = `Marco`;
-// console.log(name);
+let name = `Carlo`;
+name = `Marco`;
+console.log(name);
 
 // Esercizio 4
 
-let greet = "Ciao, Mondo!";
-console.log(greet);
+// let greet = "Ciao, Mondo!";
+// console.log(greet);
 
 // Esercizio 4/b
+let greet = `Ciao, Mondo!`;
+greet = `Come stai, Mondo`;
+console.log(greet);
+
 
 // Esercizio 4/c
