e3e6fd7399d8f85f85c90b51fd0498d741ebc8c9
zGiusee <giusebelforte@gmail.com>
2023-11-22 10:29:56 +0100

    Quick class exercise n'7 done

--- js/script.js
+++ js/script.js
@@ -1,52 +1,82 @@
-// Esercizio 1
-let number = 100;
-console.log(number); 
+// // Esercizio 1
+// let number = 100;
+// console.log(number); 
 
-// Esercizio 2
-const pi = 3.14;
-console.log(pi);
+// // Esercizio 2
+// const pi = 3.14;
+// console.log(pi);
 
-// Esercizio 2/B
-let radius = 8;
-console.log(radius)
+// // Esercizio 2/B
+// let radius = 8;
+// console.log(radius)
 
-// Esercizio 2/C
-let circle = radius * 2 * pi;
-console.log(circle)
+// // Esercizio 2/C
+// let circle = radius * 2 * pi;
+// console.log(circle)
 
-// Esercizio 3
-let name = `Carlo`;
-name = `Marco`;
-console.log(name);
+// // Esercizio 3
+// let name = `Carlo`;
+// name = `Marco`;
+// console.log(name);
 
-// Esercizio 4
+// // Esercizio 4
+
+// // let greet = "Ciao, Mondo!";
+// // console.log(greet);
+
+// // Esercizio 4/b
 
 // let greet = "Ciao, Mondo!";
+
+// // let greet = "Come stai, Mondo";
+// // console.log(greet);
+
+
+// // Esercizio 4/c
+
+// greet = `${greet} ${"Come stai?"}`
 // console.log(greet);
 
-// Esercizio 4/b
+// // Esercizio 5
+// let num= "20"; 
+// let num2= "10";
+// let comparison;
 
-let greet = "Ciao, Mondo!";
+// if(num < num2){
+//     comparison = num;
+// }
+// else{
+//     comparison = num2;
+// }
 
-// let greet = "Come stai, Mondo";
-// console.log(greet);
+// console.log(comparison);
 
+// Esercizio 6
+// let comparison;
+// let num1 = 60;
+// let num = 60;
 
-// Esercizio 4/c
+// if(num = num1){
 
-greet = `${greet} ${"Come stai?"}`
-console.log(greet);
+//     comparison = "le scritte sono uguali"
+// }
+// else{
+//     comparison = "le scritte non sono uguali"
+// }
 
-// Esercizio 5
-let num= "20"; 
-let num2= "10";
+// console.log(comparison);
+
+// Esercizio 7
+
+let num = 10;
 let comparison;
 
-if(num < num2){
-    comparison = num;
+if ("10" == num){
+    comparison = true;
 }
+
 else{
-    comparison = num2;
+    comparison = false;
 }
 
 console.log(comparison);
